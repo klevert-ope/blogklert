5468f4648b3e0ef1989170bb9b0218d0fe859ba5
Klevert Opee <81738300+klevert-ope@users.noreply.github.com>
2024-03-07 04:30:16 +0300

    refactor(code): Added an origin

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,7 +8,7 @@ import (
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Allow CORS
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173, https://klevertopee.com")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
